bc072408c2d60ef2fd9840278063a41cb50b133d
DAMON CIARELLI <gdamon@gmail.com>
2022-03-28 20:29:33 -0700

    pypi stuff

---
+++ nucypher_ops/__about__.py
@@ -0,0 +1,16 @@
+__title__ = "nucypher-ops"
+
+__url__ = "https://github.com/nucypher/nucypher-ops"
+
+__summary__ = 'Install and management tools for a proxy re-encryption network to empower privacy in decentralized systems.'
+
+__version__ = "0.1.0"
+
+__author__ = "NuCypher"
+
+__email__ = "dev@nucypher.com"
+
+__license__ = "GNU Affero General Public License, Version 3"
+
+__copyright__ = 'Copyright (C) 2022 NuCypher'
+
--- setup.py
+++ setup.py
@@ -1,9 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from setuptools import setup
+from pathlib import Path
+from typing import Dict
+
+
+
+
+PACKAGE_NAME = 'nucypher_ops'
+BASE_DIR = Path(__file__).parent
+
+ABOUT: Dict[str, str] = dict()
+SOURCE_METADATA_PATH = BASE_DIR / PACKAGE_NAME / "__about__.py"
+with open(str(SOURCE_METADATA_PATH.resolve())) as f:
+    exec(f.read(), ABOUT)
+
+
+PYPI_CLASSIFIERS = [
+      "Development Status :: 3 - Alpha",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+      "Natural Language :: English",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 3 :: Only",
+      "Programming Language :: Python :: 3.7",
+      "Programming Language :: Python :: 3.8",
+      "Programming Language :: Python :: 3.9",
+      "Topic :: Security"
+]
 
 setup(
-    name='nucypher-ops',
-    version='0.1',
-    py_modules=['nucypher-ops'],
+    py_modules=['nucypher_ops'],
+    name=ABOUT['__title__'],
+    url=ABOUT['__url__'],
+    version=ABOUT['__version__'],
+    author=ABOUT['__author__'],
+    author_email=ABOUT['__email__'],
+    description=ABOUT['__summary__'],
+    license=ABOUT['__license__'],
     install_requires=[
         'click',
         'colorama',
